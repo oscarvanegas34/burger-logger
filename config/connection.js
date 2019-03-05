59448e9ee13ef3942a60e144f52a50c07e8864fa
oscarvanegas34 <odvanegas@gmail.com>
2019-03-05 18:26:24 -0500

    Connecting Database with Jaws

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Sabroso@32",
-  database: "burgers_db"
-});
-
 // var connection = mysql.createConnection({
+//   host: "localhost",
 //   port: 3306,
-//   host: 'h2cwrn74535xdazj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//   user: 'juj0mn01g5cse60f',
-//   password: "xy2ki3dmnjdip7yk",
-//   database: "ticikt0vkm7hco7g" 
+//   user: "root",
+//   password: "Sabroso@32",
+//   database: "burgers_db"
 // });
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: 'e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'u6r7jt22doygdf6j',
+  password: "yw2aq94i2bvcwh99",
+  database: "u16posidodimxokx" 
+});
 
 
 // Make connection.
